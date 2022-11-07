6b6f59a1e77d44c7e3d6b0304d4e53e754370ba3
leskevich <leskevichtema@gmail.com>
2022-11-07 18:38:25 +0100

    lesson...21(Верстаем страницу Диалогов)

--- src/Components/Dialogs/Dialogs.tsx
+++ src/Components/Dialogs/Dialogs.tsx
@@ -3,8 +3,19 @@ import s from './Dialogs.module.css'
 
 export const Dialogs = () => {
     return (
-        <div>
-            dialog
+        <div className={s.dialogs}>
+            <div className={s.dialogsName}>
+                <div className={s.name + ' ' + s.active}>roma</div>
+                <div className={s.name}>vadim</div>
+                <div className={s.name}>anton</div>
+                <div className={s.name}>andrei</div>
+            </div>
+            <div className={s.dialogMessage}>
+                <div className={s.message}>hello1</div>
+                <div className={s.message}>hello</div>
+                <div className={s.message}>hello</div>
+                <div className={s.message}>hello</div>
+            </div>
         </div>
     );
 };
